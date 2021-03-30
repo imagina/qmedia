2c07b250eea54e4035f49bd249391c2991c571a8
msolanogithub <msolano2505@gmail.com>
2021-03-30 16:18:20 -0500

    +Update: qmedia | Set page `selectCKEditor` as mainPage

--- _config/adminPages.js
+++ _config/adminPages.js
@@ -11,16 +11,4 @@ export default {
     icon: 'fas fa-camera-retro',
     authenticated: true,
   },
-  //Home Page
-  selectMediackEditor: {
-    permission: 'media.medias.index',
-    activated: true,
-    path: '/media/select',
-    name: 'app.media.select',
-    page: () => import('@imagina/qmedia/_pages/admin/selectCkEditor'),
-    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
-    title: 'qmedia.sidebar.adminIndex',
-    icon: 'fas fa-camera-retro',
-    authenticated: true,
-  },
 }
---
+++ _config/mainPages.js
@@ -0,0 +1,14 @@
+export default {
+  //Home Page
+  selectMediackEditor: {
+    permission: 'media.medias.index',
+    activated: true,
+    path: '/media/select',
+    name: 'app.media.select',
+    page: () => import('@imagina/qmedia/_pages/admin/selectCkEditor'),
+    layout: () => import('@imagina/qsite/_layouts/blank.vue'),
+    title: 'qmedia.sidebar.adminIndex',
+    icon: 'fas fa-camera-retro',
+    authenticated: true,
+  },
+}
