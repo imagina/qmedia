e4229e62abbde24c6264f9b5b56a34e4794525c9
msolano <msolano@imaginacolombia.com>
2022-05-18 19:05:51 -0500

    +Add: extra field to zone CRUD

--- _crud/zones.vue
+++ _crud/zones.vue
@@ -7,6 +7,18 @@ export default {
     }
   },
   computed: {
+    //Get settings
+    settings() {
+      return {
+        imagesType: this.$store.getters['qsiteApp/getSettingValueByName']('media::allowedImageTypes'),
+        filesType: this.$store.getters['qsiteApp/getSettingValueByName']('media::allowedFileTypes'),
+        videosType: this.$store.getters['qsiteApp/getSettingValueByName']('media::allowedVideoTypes'),
+        audiosType: this.$store.getters['qsiteApp/getSettingValueByName']('media::allowedAudioTypes'),
+        ratios: this.$store.getters['qsiteApp/getSettingValueByName']('media::allowedRatios'),
+        maxFileSize: this.$store.getters['qsiteApp/getSettingValueByName']('media::maxFileSize'),
+      }
+    },
+    //Crud Data
     crudData() {
       return {
         crudId: this.crudId,
@@ -30,7 +42,27 @@ export default {
             },
             {name: 'zone', label: this.$tr('isite.cms.label.zone'), field: 'name', align: 'rigth'},
             {
+              name: 'accept', label: this.$tr('isite.cms.label.accept'), field: 'options', align: 'rigth',
+              format: val => val.accept || "-"
+            },
+            {
+              name: 'ratio', label: this.$tr('isite.cms.label.ratio'), field: 'options', align: 'rigth',
+              format: val => val.ratio || "-"
+            },
+            {
+              name: 'maxFileSize', label: this.$tr('media.cms.maxFileSize'), field: 'options', align: 'rigth',
+              format: val => val.maxFileSize || "-"
+            },
+            {
+              name: 'maxFiles', label: this.$tr('media.cms.maxFiles'), field: 'options', align: 'rigth',
+              format: val => val.maxFiles || "-"
+            },
+            {
               name: 'created_at', label: this.$tr('isite.cms.form.createdAt'), field: 'createdAt', align: 'left',
+              format: val => val ? this.$trd(val) : '-',
+            },
+            {
+              name: 'updatedAt', label: this.$tr('isite.cms.form.updatedAt'), field: 'updatedAt', align: 'left',
               format: val => val ? this.$trd(val) : '-',
             },
             {name: 'actions', label: this.$tr('isite.cms.form.actions'), align: 'left'},
@@ -59,14 +91,76 @@ export default {
             require: true,
             props: {
               label: `${this.$tr('isite.cms.label.entity')}*`,
-              disableBranchNodes: true
+              disableBranchNodes: true,
+              rules: [
+                val => !!val || this.$tr('isite.cms.message.fieldRequired')
+              ],
             },
             loadOptions: {
               apiRoute: "apiRoutes.qsite.modulesInfo",
               requestParams: {filter: {asSelect: true}},
               select: {label: "name", id: "path"}
             }
-          }
+          },
+          accept: {
+            value: "all",
+            type: "select",
+            fakeFieldName: "options",
+            props: {
+              label: this.$trp("isite.cms.label.accept"),
+              options: [
+                {
+                  label: `${this.$tr("isite.cms.label.all")}`,
+                  value: "all"
+                },
+                {
+                  label: `${this.$trp("isite.cms.label.image")} (${this.settings.imagesType.join(",")})`,
+                  value: "images"
+                },
+                {
+                  label: `${this.$trp("isite.cms.label.file")} (${this.settings.filesType.join(",")})`,
+                  value: "files"
+                },
+                {
+                  label: `${this.$trp("isite.cms.label.video")} (${this.settings.videosType.join(",")})`,
+                  value: "videos"
+                },
+                {
+                  label: `${this.$trp("isite.cms.label.audio")} (${this.settings.audiosType.join(",")})`,
+                  value: "audios"
+                },
+              ]
+            }
+          },
+          ratio: {
+            value: "free",
+            type: "select",
+            fakeFieldName: "options",
+            props: {
+              label: this.$tr("isite.cms.label.ratio"),
+              options: this.settings.ratios.map(ratio => {
+                return {label: ratio, value: ratio}
+              })
+            }
+          },
+          maxFileSize: {
+            value: this.settings.maxFileSize || 10,
+            type: "input",
+            fakeFieldName: "options",
+            props: {
+              label: this.$tr('media.cms.maxFileSize'),
+              type: "number"
+            }
+          },
+          maxFiles: {
+            value: 1,
+            type: "input",
+            fakeFieldName: "options",
+            props: {
+              label: this.$tr('media.cms.maxFiles'),
+              type: "number"
+            }
+          },
         },
         formRight: {
           mediasSingle: {
