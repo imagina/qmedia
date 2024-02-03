8c1d0db26e83f5f89470e6bd43940a9671eb1b7a
msolanogithub <msolano@imaginacolombia.com>
2024-02-03 13:03:58 -0500

    Temporal comment dependencies imports

--- _services/index.js
+++ _services/index.js
@@ -1,5 +1,5 @@
 import crud from '@imagina/qcrud/_services/baseService'
-import http from "axios/index";
+import http from "axios";
 import remember from "@imagina/qsite/_plugins/remember"
 import config from '@imagina/qsite/_config/master/index'
 
