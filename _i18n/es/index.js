8a4b3f051c1ae020d9a15fb48951fcad8c9868cb
hrodriguezimagina <hrodriguez@imaginacolombia.com>
2024-06-25 17:01:41 -0500

    + added translations

--- _i18n/es/index.js
+++ _i18n/es/index.js
@@ -1,7 +1,47 @@
-import layout from 'modules/qmedia/_i18n/es/layout'
-import sidebar from 'modules/qmedia/_i18n/es/sidebar'
-
 export default {
-  layout,
-  sidebar
+  "media": {
+    "cms": {
+      "sidebar": {
+        "adminGroup": "Media",
+        "adminIndex": "Lista de Archivos",
+        "adminZones": "Zonas de Archivos"
+      },
+      "title": "Multimedia Listar/Buscar",
+      "newFile": "Nuevo Archivo",
+      "updateFile": "Actualizar Archivo",
+      "newFolder": "Nueva Carpeta",
+      "updateFolder": "Actualizar Carpeta",
+      "newImage": "Nueva Imagen",
+      "updateImage": "Actualizar Imagen",
+      "renameFolder": "Renombrar Carpeta",
+      "uploadFile": "Cargar Archivo",
+      "editFile": "Editar Archivo",
+      "moveMedia": "Mover Media",
+      "selectMedia": "Seleccionar Media",
+      "form": {
+        "altAttribute": "Attributo Alt",
+        "keyWords": "Palabras Claves",
+        "folderName": "Nombre de la carpeta",
+        "moveTo": "Mover a"
+      },
+      "messages": {
+        "addYourFiles": "Agrega tus archivos aquí",
+        "addYourImages": "Agrega tus imagenes aquí",
+        "limitFiles": "Limite de Archivos",
+        "messageLimitFiles": "El limite de archivos es de <b>{quantity}</b>. para seleccionar otro archivo primero debes quitar algunos de los seleccionados",
+        "failedUploadFiles": "Error al cargar los archivos",
+        "errorMaxFileSize": "Los Archivos superan el tamaño permitido de {size}MB"
+      },
+      "newZone": "Nueva Zona",
+      "updateZone": "Actualizar Zona",
+      "maxFileSize": "Tamaño maximo por archivo (MB)",
+      "maxFiles": "Cantidad maxima de archivos seleccionados",
+      "directUpload": "Subida directa",
+      "thumbnailsConfig": "Configuración de Miniaturas",
+      "documentation": {
+        "medias": "Aquí se almacenan todos los archivos que se cargan en tu aplicación, y se ordenan por carpetas",
+        "zones": "La aplicación identifica los archivos relacionados con otros módulos por zonas, estas zonas definen cuantos archivos y de que manera seleccionarlo en relación"
+      }
+    }
+  }
 }
--- _i18n/es/sidebar.js
+++
@@ -1,4 +0,0 @@
-export default {
-  adminGroup: 'Media',
-  adminIndex: 'Lista de Archivos',
-}
